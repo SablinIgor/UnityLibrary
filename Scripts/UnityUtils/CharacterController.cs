6da73a784ecb8fbb681dae9c74f5d10a3b447a74
SablinIgor <sablin.igor@gmail.com>
2021-02-21 21:23:54 +0300

    first commit

---
+++ Scripts/UnityUtils/CharacterController.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+namespace UnityUtils
+{
+    public class CharacterController : MonoBehaviour
+    {
+        private void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
