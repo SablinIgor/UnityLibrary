90c10bebcfe0bb73e5dff769970553e59e42c118
SablinIgor <sablin.igor@gmail.com>
2021-02-22 13:40:09 +0300

    Fix docfx

--- Scripts/UnityUtils/MyUtilities.cs
+++ Scripts/UnityUtils/MyUtilities.cs
@@ -2,14 +2,32 @@ using UnityEngine;
 
 namespace UnityUtils
 {
+    /// <summary>
+    /// The main MyUtilities class.
+    /// Contains all methods for performing basic math functions.
+    /// </summary>
     public class MyUtilities {
 
         public int c;
 
+        /// <summary>
+        /// Adds two integers and returns the result.
+        /// </summary>
+        /// <param name="a">A int precision number.</param>
+        /// <param name="b">A int precision number.</param>
+        /// <returns>
+        /// The sum of two integers.
+        /// </returns>
         public void AddValues(int a, int b) {
             c = a + b;
         }
 
+        /// <summary>
+        /// Generate random number.
+        /// </summary>
+        /// <param name="min">A lower limit of range.</param>
+        /// <param name="max">A upper limit of range.</param>
+        /// <returns>Random number</returns>
         public static int GenerateRandom(int min, int max) {
             System.Random rand = new System.Random();
             return rand.Next(min, max);
